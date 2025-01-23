4d1996293d116d6221d798e33e148aa20018a5fd
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-01-23 12:22:54 +0000

    lisasin Card abikomponendi ExpensesItem-isse

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
+import Card from "./Card.jsx"
 import ExpenseDate from "./ExpenseDate.jsx"
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <div>
                 <h1 className="expense-item__name">{props.data.name}</h1>
                 <p className="expense-item__description">{props.data.description}</p>
                 <p className="expense-item__date">{props.data.price}</p>
             </div>
             <ExpenseDate data={props.data} />
-        </div>
+        </Card>
     )
 }
 
