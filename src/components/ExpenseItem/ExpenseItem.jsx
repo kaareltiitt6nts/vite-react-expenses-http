2b70a32b696f749da43fbfc0c33868c00aa5d30c
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-01-23 11:00:27 +0000

    lisasin ExpenseItem stiilid, lisasin ExpenseDate ExpenseItem komponendile juurde

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,11 +1,18 @@
+import "./ExpenseItem.css"
+import "./ExpenseDate.css"
+
 const ExpenseItem = (props) => {
     console.log(props.data)
     return (
         <div className="expense-item">
-            <h1 className="expense-item__name">{props.data.name}</h1>
             <div>
-                <h2 className="expense-item__date">{props.data.date.toDateString()}</h2>
+                <h1 className="expense-item__name">{props.data.name}</h1>
                 <p className="expense-item__description">{props.data.description}</p>
+            </div>
+            <div className="expense-date">
+                <div className="expense-date__month">{props.data.date.toLocaleString("default", {month:"long"})}</div>
+                <div className="expense-date__day">{props.data.date.getDay()}</div>
+                <div className="expense-date__year">{props.data.date.getYear()}</div>
             </div>
         </div>
     )
