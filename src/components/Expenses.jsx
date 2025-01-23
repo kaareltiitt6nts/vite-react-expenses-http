69372ac96390af2a8be55a68ff5b86e700cda33c
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-01-23 12:20:32 +0000

    tegin Card abikomponendi ning kasutasin teda Expenses loomisel

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,15 +1,16 @@
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem.jsx"
+import Card from "./Card.jsx"
 
 const Expenses = (props) => {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {
                 props.data.map(item => (
                     <ExpenseItem key={item.id} data={item} />
                 ))
             }
-        </div>
+        </Card>
     )
 }
 
